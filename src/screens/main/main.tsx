a33cfa94bdd1ae47c2d662e4b1286aa4a3f0d33b
İlker Kesici <ilker.kesici@deltasmart.tech>
2020-09-29 23:20:00 +0300

    configure run scripts

--- src/screens/main/main.tsx
+++ src/screens/main/main.tsx
@@ -1,13 +1,13 @@
-  
+
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { styles } from './main.styles';
 
-interface ImainState{ }
-interface ImainProps{ }
+interface ImainState { }
+interface ImainProps { }
 
 export const Main = (props: ImainProps) => {
-    const [state, setState] = useState<ImainState>({ });
+    const [state, setState] = useState<ImainState>({});
 
     return (
         <View style={styles.container}>
